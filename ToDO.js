5542dd51180d55a790fe29c268eab6d9f7a86e78
김휘겸 <breakstorm00@gmail.com>
2017-06-27 10:14:42 +0900

    Modify 화면 레이아웃 완성
     Changes to be committed:
    	modified:   ToDO.js
    	modified:   secondToDo.html

--- ToDO.js
+++ ToDO.js
@@ -1,2 +1,7 @@
 console.log("hello world");
-var add
+var add = document.querySelector(".add");
+var order = document.querySelector(".order")
+var list = order.querySelectorAll("li")
+console.log(add);
+console.log(order);
+console.log(list);
