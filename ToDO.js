e784d33cd52a34da5125ba6087af45229b3d64f9
김휘겸 <breakstorm00@gmail.com>
2017-06-27 15:16:03 +0900

    Modify addList function on addEventListener
     Changes to be committed:
    	modified:   ToDO.js

--- ToDO.js
+++ ToDO.js
@@ -1,7 +1,29 @@
 console.log("hello world");
+
+
 var add = document.querySelector(".add");
-var order = document.querySelector(".order")
-var list = order.querySelectorAll("li")
-console.log(add);
-console.log(order);
-console.log(list);
+var order = document.querySelector(".order");
+var list = order.querySelectorAll("li");
+var input = document.querySelector("input");
+
+
+// console.log(add);
+// console.log(order);
+// console.log(list);
+
+function addList (evt){
+	// debugger;
+	// console.log(this);
+	// console.log(evt)
+	// console.log(arguments);
+
+	if(evt.target.tagName === "BUTTON" || evt.keyCode === 13){
+	var temp = input.value;
+	var tempHTML = '<li class="fl-left">' + temp + '</li><button class="fl-right">변경</button><button class="fl-right">완료</button><button class="fl-right">삭제</button><br>'
+	order.lastElementChild.insertAdjacentHTML('afterend',tempHTML);
+	input.value = "";
+	}
+}
+
+add.addEventListener("click", addList);
+input.addEventListener("keydown", addList);
